0a71037f8edbef5d038800ca4f9b80c16e8b84f5
Clement Denis <le.mikmac@gmail.com>
2018-01-25 21:11:36 +0100

    add back

--- back/controllers/Gifts.js
+++ back/controllers/Gifts.js
@@ -1,16 +1,55 @@
+const fs = require('fs');
+const { promisify } = require('util');
+const readFile = promisify(fs.readFile);
+const writeFile = promisify(fs.writeFile);
+const giftPath = process.env.GIFTS_STORE_PATH || './gifts.json';
+const nodemailer = require('nodemailer');
+const mailgunTransport = require('nodemailer-mailgun-transport');
+
+const mailer = nodemailer.createTransport(mailgunTransport({
+  auth: {
+    api_key: process.env.MAILGUN_APIKEY,
+    domain: process.env.MAILGUN_DOMAIN
+  },
+  debug: true,
+}));
+
+try {
+  fs.statSync(giftPath);
+} catch (err) {
+  fs.writeFileSync(giftPath, '[]', 'utf8');
+}
+
+const saveGifts = gifts => writeFile(giftPath, JSON.stringify(gifts), 'utf8')
+const readGifts = async () => JSON.parse(await readFile(giftPath, 'utf8'))
+'use strict';
+
 const Gifts = {
-    create: (req, res, next) => {
-
-    },
-    read: (req, res, next) => {
-
-    },
-    delete: (req, res, next) => {
-
-    },
-    notify: (req, res, next) => {
-        // Send a mail to Santa 
-    }
+  create: async req => {
+    const gifts = await readGifts();
+    const lastGift = gifts[gifts.length - 1];
+    gifts.push({
+      name: String(req.body.name),
+      key: lastGift ? lastGift.key + 1 : 0
+    });
+    await saveGifts(gifts);
+    return gifts;
+  },
+  read: readGifts,
+  delete: async req => {
+    gifts = (await readGifts())
+      .filter(gift => gift.key !== req.body.key);
+    await saveGifts(gifts);
+    return gifts;
+  },
+  notify: async () => mailer.sendMail({
+    from: `Le Père Noel 🎅 <santa@${process.env.MAILGUN_DOMAIN}>`, // sender address
+    subject: "🎄Votre liste de souhaits 🎁", // Subject line
+    to: 'le.mikmac@gmail.com',
+    text: (await readGifts())
+        .map(gift => gift.name)
+        .join('\n'),
+  })
 }
 
 module.exports = Gifts;
--- back/routes/index.js
+++ back/routes/index.js
@@ -2,10 +2,18 @@ const express = require('express');
 const router = express.Router();
 const Gifts = require('../controllers/Gifts');
 
+const asyncRoute = fn => (req, res, next) => fn(req)
+  .then(answer => res.json(answer))
+  .catch(next);
+
 router
   .route('/')
-  .get(Gifts.read)
-  .post(Gifts.create)
-  .delete(Gifts.delete);
+  .get(asyncRoute(Gifts.read))
+  .post(asyncRoute(Gifts.create))
+  .delete(asyncRoute(Gifts.delete));
+
+router
+  .route('/notify')
+  .get(asyncRoute(Gifts.notify));
 
 module.exports = router;
